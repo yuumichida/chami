c9e4fc0bc84004324fd80ab7a485b630ad52c1fa
yuumichida <167832837+yuumichida@users.noreply.github.com>
2024-10-15 10:15:54 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -1,6 +1,2 @@
-function sayhello61(){
-  alert('Hello,Function!');
-}
-function saygoodbye61(){
-  alert('Goodbye.');
-}
+function sayhello61(){alert('Hello,Function!');}
+function saygoodbye61(){alert('Goodbye!');}
