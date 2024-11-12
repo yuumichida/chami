829f1925d8eac4ced71c1223879cad62218a3cfd
yuumichida <167832837+yuumichida@users.noreply.github.com>
2024-11-12 13:13:03 +0900

    Create ex6-10.js

---
+++ ex6-10.js
@@ -0,0 +1,6 @@
+function imgchange1() {
+    document.getElementById('logo').src = './image/josai.jpg';
+}
+function imgchange2() {
+    document.getElementById('logo').src = './image/josai.jpg';
+}
