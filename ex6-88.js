dff9a616db6460ff8ec4fd4ef53b3daf68f9e2d4
yuumichida <167832837+yuumichida@users.noreply.github.com>
2024-11-12 13:11:28 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
- function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/rain.png" alt="オン">';}
+ function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/nikoniko.png" alt="オン">';}
