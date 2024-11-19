8f8032cef3a77673868b3c32b649aeb2d1346c00
yuumichida <167832837+yuumichida@users.noreply.github.com>
2024-11-19 22:43:41 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,6 +1,2 @@
-function imgchange1() {
-    document.getElementById('mark').src = './image/naku.png';
-}
-function imgchange2() {
-    document.getElementById('mark').src = './image/nikoniko.png';
-}
+function imgchange1() {document.getElementById('mark').src = './image/naku.png';}
+function imgchange2() {document.getElementById('mark').src = './image/nikoniko.png';}
