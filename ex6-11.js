a1e6d65a08a50fe2c48b5fe3e251cfc8adea7076
yuumichida <167832837+yuumichida@users.noreply.github.com>
2024-11-19 22:44:06 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,6 +1,2 @@
-function imgchange3(){
-  document.getElementById('logo').src='./image/jousaikyara.jpg';
-}
-function imgchange4(){
-  document.getElementById('logo').src='./image/josailogo.jpg';
-}
+function imgchange3(){document.getElementById('logo').src='./image/jousaikyara.jpg';}
+function imgchange4(){document.getElementById('logo').src='./image/josailogo.jpg';}
