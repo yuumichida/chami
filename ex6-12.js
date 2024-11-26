49936adac840e958c0dc01727807480d0e2ce4f7
yuumichida <167832837+yuumichida@users.noreply.github.com>
2024-11-26 09:42:38 +0900

    Create ex6-12.js

---
+++ ex6-12.js
@@ -0,0 +1,9 @@
+function hyoji(){//表示する文字var str="一文字ずつ表示します。";
+//テキストボックスの文字数
+var cn=document.timer.moji.value.length;
+//文字が全部表示されているか確認
+if(cnt<11){//現在より1文字多く切り出して表示
+document.timer.moji.value="";}}
+function startfnc(){
+//関数hyoji()を1000ミリ秒間隔で呼び出す
+stelnterval("hyoji()",1000;}
