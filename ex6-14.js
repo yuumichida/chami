0dea8e353c6f1bcb6b084e0c57836259fc1050f5
yuumichida <167832837+yuumichida@users.noreply.github.com>
2024-12-03 10:29:27 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,6 +1,1 @@
-//画像を新しい画像に切り替える
-function imgchange1(){document.getElementById('mark').src='./image/naku.png';}
-
-settimeout((){document.getElementById('mark').src='./image/nikoniko.png';},3000);}
-
 
