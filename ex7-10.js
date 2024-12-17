c53085584d28e2fd3963c285ad98de633fd6e191
yuumichida <167832837+yuumichida@users.noreply.github.com>
2024-12-17 10:07:13 +0900

    Create ex7-10.js

---
+++ ex7-10.js
@@ -0,0 +1,7 @@
+function add710(){var c=document.getElementById('input7101').value;
+                  var d=document.getElementById('input7102').value;
+                  alert(Number(c)/(Number(d)*Number(d)));
+                  var bmi=(Number(c)/(Number(d)*Number(d)));
+                  if(bmi<18.5){alert('低体重');}
+                  else if(bmi<25){alert('普通');}
+                  else{alert('肥満');}}
