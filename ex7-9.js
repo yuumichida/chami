23cf5d5dc000941744447bd90f9ae71b213c9966
yuumichida <167832837+yuumichida@users.noreply.github.com>
2024-12-17 09:55:20 +0900

    Update ex7-9.js

--- ex7-9.js
+++ ex7-9.js
@@ -1,1 +1,4 @@
+function nazo79(){if(document.getElementById('answer79').value=='夢'){alert('あたり！');}
+                  else if(document.getElementById('answer79').value=='ゆめ'){alert('あたり！');}
+                  else{alert('はずれ！');}}
 
