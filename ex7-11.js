7bd0268ab5aa9d96fbdabdabb7b6ddf78f6880cf
yuumichida <167832837+yuumichida@users.noreply.github.com>
2024-12-17 10:22:21 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,1 +1,5 @@
+function year(){var e=document.getElementById('year1').value;
+                if(Number(e)%100==0){alert('平年');}
+              　else if(Number(e)%4==0){alert('閏年');}
+              　else {alert('平年');}}
 
